d0fa4d780cebfa9b5e78dac6e6c654d474703b04
darek <darek.strulak@gmail.com>
2018-09-22 15:25:07 +0200

    templates dodane

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -1,8 +1,10 @@
 package pl.akademiakodu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.model.Gif;
 
 @Controller
 public class GifController {
@@ -11,5 +13,16 @@ public class GifController {
     //@ResponseBody //jak nie ma modelu wiec wyswietlo to co jest tu
     public String listGifts(){
         return "home";
+    }
+
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif(true, "compiler-bot", "michalos");
+        modelMap.put("gif", gif);
+        return "gif-details";
+
+
+
     }
 }
